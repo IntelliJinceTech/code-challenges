971ac16fddeedc6349ff61fd5429ed2b18443e11
Christopher Jin <chrisjin1@gmail.com>
2022-04-06 23:09:01 -0400

    sum Positives from Array

---
+++ sumOfPositive/main.js
@@ -0,0 +1,25 @@
+// You get an array of numbers, return the sum of all of the positives ones.
+
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+// Note: if there is nothing to sum, the sum is default to 0.
+
+// function positiveSum(arr) {
+//     return arr.filter(posNum => posNum>=0)
+//             .reduce ((sum,num) => sum + num,0)
+// }
+
+function positiveSum(arr) {
+    return arr.reduce((sum,num) => sum + (num>0 ? num : 0))
+}
+
+
+console.log(positiveSum([1,2,3,4,5]))
+
+console.log(positiveSum([1,-2,3,4,5]))
+
+console.log([0])
+
+console.log(positiveSum([-1,-2,-3,-4,-5]))
+
+console.log(positiveSum([-1,2,3,4,-5]))
