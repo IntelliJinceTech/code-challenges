cf3ad7b780e95d8080dda6589a6be8a08da2e9a3
Christopher Jin <chrisjin1@gmail.com>
2022-05-25 16:41:27 -0400

    1st try jadencase strings

--- 7kyus/allThe7kyus/js/jadenCaseStrings.js
+++ 7kyus/allThe7kyus/js/jadenCaseStrings.js
@@ -9,3 +9,14 @@
 //? split the string into an array
 //? make the first character of each element word uppercase
 //? join the elements together no with the updated uppercase first letters
+
+String.prototype.toJadenCase = function () {
+    let words = this.split(" ");
+    for (let i = 0; i < words.length; i++) {
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+    }
+    let updatedWords = words.join(" ");
+    return updatedWords;
+};
+
+console.log("How can mirrors be real if our eyes aren't real".toJadenCase());
