7a3f973d53fc6c3a05258db847a96c5ac622a30f
Christopher Jin <chrisjin1@gmail.com>
2022-05-31 22:15:31 -0400

    code 8kyus math functions

---
+++ restOfThe8kyus/js/countOddNumbersBelowN.js
@@ -0,0 +1,10 @@
+// function oddCount(n) {
+//     if (n % 2 === 1) {
+//         return (n - 1) / 2
+//     } else {
+//         return n / 2
+//     }
+// }
+function oddCount(n) {
+    return n % 2 === 1 ? (n - 1) / 2 : n / 2
+}
