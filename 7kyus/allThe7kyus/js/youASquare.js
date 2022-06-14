4e15d22ce947f0dc7d57ec9d807d0bc6b2e9c44a
Christopher Jin <chrisjin1@gmail.com>
2022-06-13 21:27:36 -0400

    PREP complete Check if Square

---
+++ 7kyus/allThe7kyus/js/youASquare.js
@@ -0,0 +1,23 @@
+// P - Parameters
+//? Parameter will be an integer
+//? can be positive or negative
+//? 0 could be a possibility
+// R - Return 
+//? Returns a boolean true or false 
+//? 
+//? 
+// E - Examples
+//? -1  =>  false
+//?  0  =>  true
+//?  3  =>  false
+//?  4  =>  true
+//? 25  =>  true
+//? 26  =>  false
+// P - Pseudocode
+//? given an integer that can be 0, negative or positive
+//? if negative or sqrt of argument is a decimal - return false
+//? otherwise if squrt of argument is not a decimal - return true
+
+let isSquare = function (number) {
+    return false; // fix me
+}
