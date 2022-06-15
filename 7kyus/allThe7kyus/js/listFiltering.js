504ca51e7695a11afdd05454cd314889e3f42717
Christopher Jin <cjin@Christophers-MacBook-Pro-2.local>
2022-06-15 13:51:54 -0400

    list filtering strings numbers prep complete

---
+++ 7kyus/allThe7kyus/js/listFiltering.js
@@ -0,0 +1,21 @@
+// P - Parameters
+//? array of strings and non-negative integers
+// R - Return 
+//? returns a new array
+//?     without only the non-negative integers
+//?     type should be number
+
+// E - Examples
+//? [1,2,'a','b']) == [1,2]
+// [1,'a','b',0,15]) == [1,0,15]
+// [1,2,'aasf','1','123',123]) == [1,2,123]
+// P - Pseudocode
+//? Given array of strings and numbers
+//? Return a new array that
+//?     filters out typeof element is string
+//?     filters out numbers > = 0
+
+
+function filter_list(l) {
+    // Return a new array with the strings filtered out
+}
