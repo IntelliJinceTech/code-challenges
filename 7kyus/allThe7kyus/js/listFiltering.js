365dfc970be089bef61dcf090d878917d7369ff1
Christopher Jin <cjin@Christophers-MacBook-Pro-2.local>
2022-06-15 14:16:09 -0400

    list Filtering complete with reflection

--- 7kyus/allThe7kyus/js/listFiltering.js
+++ 7kyus/allThe7kyus/js/listFiltering.js
@@ -15,7 +15,21 @@
 //?     filters out typeof element is string
 //?     filters out numbers > = 0
 
+//! first iteration
+// function filter_list(arr) {
+//     return arr.filter((ele) => {
+//         return typeof ele == 'number' && ele >= 0;
+//     })
+// }
 
-function filter_list(l) {
-    // Return a new array with the strings filtered out
+//! second iteration
+function filter_list(arr) {
+    return arr.filter((ele) => typeof ele == 'number' && ele >= 0)
 }
+
+// console.log(filter_list([1, 2, 'a', 'b']))
+// console.log(filter_list([1, 'a', 'b', 0, 15]))
+console.log(filter_list([1, 2, 'aasf', '1', '123', 123]))
+
+//reflections
+// There is no need to cover the array element is non-negative since parameters cover that already
