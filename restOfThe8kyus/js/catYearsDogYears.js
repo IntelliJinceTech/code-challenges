7ce10b174d222c6e5a102faf3fbfd8f3f70c66b9
Christopher Jin <chrisjin1@gmail.com>
2022-06-16 11:32:43 -0400

    prep catYearsDogYears

---
+++ restOfThe8kyus/js/catYearsDogYears.js
@@ -0,0 +1,15 @@
+// function parameters will be integers representing human years
+//  always integers, non-negative, numbers greater than or equal to 1
+// return an array of 3 elements. each element is a number
+// examples:
+//  humanYearsCatYearsDogYears(1), [1,15,15]
+//  humanYearsCatYearsDogYears(2), [2,24,24]
+//  humanYearsCatYearsDogYears(10), [10,56,64]
+// ? given a parameter
+// ? declare variables for both catYears and dogYears
+// ? set both variables to 15
+// ? if argument = 1, return array with [argument, catYears,dogYears]
+// ? if argument = 2, add 9 to both variables
+// ? if argument >2, loop through starting at 3, each iteration adding 4 to catYears, 5 to dogYears
+// ? return [argument, catYears,dogYears]
+// ?
