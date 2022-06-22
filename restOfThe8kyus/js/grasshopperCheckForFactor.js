329e6d25b4e734d47be5370156fba8b094263773
Christopher Jin <chrisjin1@gmail.com>
2022-06-21 22:17:05 -0400

    8kyu grasshopper

---
+++ restOfThe8kyus/js/grasshopperCheckForFactor.js
@@ -0,0 +1,11 @@
+// 2 parameters, both numbers
+// return boolean
+//
+// ! given 2 parameters, base and factor
+// !    return boolean based on whether base is divisible by factor
+
+// !
+
+function checkForFactor(base, factor) {
+    return base % factor == 0;
+}
