402e455313069d38daa40d84d74529c72b4afacf
Christopher Jin <cjin@Christophers-MacBook-Pro-2.local>
2022-06-22 17:25:38 -0400

    PREP complete for xs and os

---
+++ 7kyus/allThe7kyus/js/exesAndOhs.js
@@ -0,0 +1,36 @@
+// function would intake parameters of:
+//      string that could consist of x's and o's 
+// returns boolean (true or false)
+//      based on whether number of x's and o's equal per string parameter
+// Examples include:
+//? XO("ooxx") => true
+//? XO("xooxx") => false
+//? XO("ooxXm") => true
+//? XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// given a string
+// determine if x's and o's not present --> return true 
+//      This could possibly be solved for anyways in logic
+// create a counter for o's
+// create a counter for x's
+// make string all lower case
+// split the string into an array and for Each of the elements within array, if matching x or o, increase counter for corresponding letter
+// return true if x's and o's equal otherwise false
+
+function XO(str) {
+    let xCounter = 0;
+    let oCounter = 0;
+    str.toLowerCase().split('').forEach(element => {
+        if (element === 'x') {
+            xCounter++
+        } else if (element === 'o') {
+            oCounter++;
+        }
+    });
+    return xCounter === oCounter
+}
+
+console.log(XO("ooxx")) // => true
+console.log(XO("xooxx")) // => false
+console.log(XO("ooxXm")) // => true
+console.log(XO("zpzpzpp")) // => true // when no 'x' and 'o' is present should return true
+console.log(XO("zzoo")) // => false
