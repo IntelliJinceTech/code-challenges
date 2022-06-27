6e281cd6d67ae8a7955d9fd59f495d201218e265
Christopher Jin <chrisjin1@gmail.com>
2022-06-26 23:13:14 -0400

    prep complete for shortest word 7kyu

---
+++ 7kyus/allThe7kyus/js/shortestWord.js
@@ -0,0 +1,7 @@
+// Arguments will include a string of words
+// Return an integer, typeof is number
+// Example: "bitcoin take over the world maybe who knows perhaps"), 3)
+// ! Given a string of words
+// ! split string into an array
+// ! Find the length of each element/word
+// ! Return the max of said array
