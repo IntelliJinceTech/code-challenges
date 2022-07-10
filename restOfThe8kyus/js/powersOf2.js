6b1c5b158ac8f14d829ba9357fb08a9620136534
Christopher Jin <chrisjin1@gmail.com>
2022-07-09 22:23:22 -0400

    pseudocode prep done powers of 2

---
+++ restOfThe8kyus/js/powersOf2.js
@@ -0,0 +1,14 @@
+// parameter is an integer
+// returning an array of integers
+// n = 0  ==> [1]        # [2^0]
+// n = 1  ==> [1, 2]     # [2^0, 2^1]
+// n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+
+//! argument to function is an integer
+//! create a loop where each time
+//!     2 to the power is incremented up by 1 until
+//!     it is equal to the number in the argument
+//!         each time pushing the number onto the array
+//! return an array of numbers
+
+function powersOfTwo(n) {}
