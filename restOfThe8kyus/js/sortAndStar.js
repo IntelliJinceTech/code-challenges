615cffa35dd95bccb31abcae92b33eca3c401b59
Christopher Jin <chrisjin1@gmail.com>
2022-07-10 23:58:40 -0400

    sort and star 8kyu prep complete

---
+++ restOfThe8kyus/js/sortAndStar.js
@@ -0,0 +1,13 @@
+// parameter of function is an array of strings
+// return one string with *** between each letter
+//twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]), 'b***i***t***c***o***i***n' )
+//
+
+//! given an array of strings
+//!     sort the strings based on case sensitivity and ASCII values
+//!     split the first string and add *** between each character
+//! return the new first string
+
+function twoSort(arrOfStr) {
+    return arrOfStr.sort();
+}
