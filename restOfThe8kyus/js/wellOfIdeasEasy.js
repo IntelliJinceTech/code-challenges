842e07d6d5f89133c0ed51c2d41b3c439dfaf980
Christopher Jin <chrisjin1@gmail.com>
2022-07-14 23:11:39 -0400

    prep done for wellofideas easy

---
+++ restOfThe8kyus/js/wellOfIdeasEasy.js
@@ -0,0 +1,7 @@
+function well(x) {
+    // create object of good and bad with values of 0
+    // loop over array and ++ when checking each element
+    // If no good ideas = fail
+    // if more than 2 = > I smell a series
+    // else publish!
+}
