1e805b845604ca188310ab66dd6d33f6729c5c8c
Christopher Jin <chrisjin1@gmail.com>
2022-07-20 23:00:54 -0400

    prep done for stringystrings

---
+++ restOfThe8kyus/js/stringyStrings.js
@@ -0,0 +1,6 @@
+function stringy(size) {
+    // given string
+    // loop over said string
+    // if element of string array is even, charat loop counter  should equal 1, otherwise 0
+    // return new string
+}
