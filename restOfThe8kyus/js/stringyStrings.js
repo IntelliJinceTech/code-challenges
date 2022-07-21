468981be955e8bbc872954f37a824dc9df6464c1
Christopher Jin <chrisjin1@gmail.com>
2022-07-20 23:07:12 -0400

    done stringyStrings

--- restOfThe8kyus/js/stringyStrings.js
+++ restOfThe8kyus/js/stringyStrings.js
@@ -3,4 +3,13 @@ function stringy(size) {
     // for loop up to given number
     // if loop counter is even, charat loop counter  should equal 1, otherwise 0
     // return string
+    let str = "";
+    for (let i = 0; i < size; i++) {
+        if (i % 2 == 0) {
+            str += 1;
+        } else {
+            str += 0;
+        }
+    }
+    return str;
 }
