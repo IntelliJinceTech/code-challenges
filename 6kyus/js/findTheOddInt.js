9ab4be1fb754976735f976eb6c16acc643efc1f5
Christopher Jin <chrisjin1@gmail.com>
2022-09-11 11:22:24 -0400

    pre findOdd

---
+++ 6kyus/js/findTheOddInt.js
@@ -0,0 +1,10 @@
+// input array of integers
+//  negatives, what does 0 count as?
+// return the integer that appears odd number of times
+//  always be at least one integer that appears an odd number of times
+
+function findOdd(arr) {}
+
+console.log(findOdd([4]), 4)
+console.log(findOdd([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1]), 4)
+console.log(findOdd([1, 2, 2, 3, 3]), 1)
