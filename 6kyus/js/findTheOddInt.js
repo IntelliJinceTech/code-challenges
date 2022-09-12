fb965d88e336cdc54ec01e44520ba20d735dda70
Christopher Jin <chrisjin1@gmail.com>
2022-09-12 11:31:20 -0400

    prep findOdd

--- 6kyus/js/findTheOddInt.js
+++ 6kyus/js/findTheOddInt.js
@@ -3,7 +3,12 @@
 // return the integer that appears odd number of times
 //  always be at least one integer that appears an odd number of times
 
-function findOdd(arr) {}
+function findOdd(arr) {
+	// todo given array of integers
+	// todo     count number of times an integer appears
+	// todo     review the counts and pick the one that satisfies:
+	// todo         number of times in arr % 2 === 1
+}
 
 console.log(findOdd([4]), 4)
 console.log(findOdd([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1]), 4)
