178b3a0b654ddf0060369c9ec8508152a9dd8fab
Christopher Jin <chrisjin1@gmail.com>
2023-01-06 09:54:24 -0500

    prep complete next perfect square first pass add examples

--- 7kyus/js/findTheNextPerfectSquare.js
+++ 7kyus/js/findTheNextPerfectSquare.js
@@ -10,8 +10,8 @@ function findNextSquare(sq) {
 	// if integer --> return integer + 1 **2
 }
 
-// console.log(Math.sqrt(625))
-// console.log(26 ** 2)
+console.log(Math.sqrt(625))
+console.log(26 ** 2)
 
 console.log(findNextSquare(121), `Expected Answer is ${144}`)
 console.log(findNextSquare(625), `Expected Answer is ${676}`)
