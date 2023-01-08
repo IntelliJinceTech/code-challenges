0c762b109757a9b5dffbdf0469a43525ecf1eac4
Christopher Jin <chrisjin1@gmail.com>
2023-01-08 10:06:23 -0500

    initial pre no pseudocode like system

---
+++ 6kyus/js/whoLikesIt.js
@@ -0,0 +1,14 @@
+//! String manipulation
+
+// takes in an array of strings --> any weirdness in the array input?
+// outputs different string combinations dependent on length of array input
+
+function likes(names) {
+	return
+}
+
+console.log(likes([]), `no one likes this`)
+console.log(likes(['Peter']), `Peter likes this`)
+console.log(likes(['Jacob', 'Alex']), `Jacob and Alex likes this`)
+console.log(likes(['Max', 'John', 'Mark']), `Max, John and Mark likes this`)
+console.log(likes(['Max', 'John', 'Mark', 'Alex']), `Max, John and 2 others likes this`)
