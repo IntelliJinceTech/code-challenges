593a513dc98565bb17a06aeabc8304a406fd19a9
Christopher Jin <chrisjin1@gmail.com>
2023-01-10 10:18:24 -0500

    breaking down the problem first pass

--- 6kyus/js/whoLikesIt.js
+++ 6kyus/js/whoLikesIt.js
@@ -7,19 +7,28 @@
 // length < 4 --> ...array likes this
 // length >=4 --> array[0], array[1] and (rest of the array).length likes this
 
-//?pseudocode
-//
-//
-//
-//
-//
-
 function likes(names) {
-	return
+	let str = ''
+	switch (names.length) {
+		case 0:
+			str = 'no one likes this'
+			break
+		case 1:
+			break
+		case 2:
+			break
+		case 3:
+			break
+		default:
+			str = `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
+			break
+	}
+	return str
 }
 
-console.log(likes([]), `no one likes this`)
-console.log(likes(['Peter']), `Peter likes this`)
-console.log(likes(['Jacob', 'Alex']), `Jacob and Alex likes this`)
-console.log(likes(['Max', 'John', 'Mark']), `Max, John and Mark likes this`)
-console.log(likes(['Max', 'John', 'Mark', 'Alex']), `Max, John and 2 others likes this`)
+// console.log(likes([]), `no one likes this`)
+// console.log(likes(['Peter']), `Peter likes this`)
+// console.log(likes(['Jacob', 'Alex']), `Jacob and Alex likes this`)
+// console.log(likes(['Max', 'John', 'Mark']), `Max, John and Mark likes this`)
+console.log(likes(['Max', 'John', 'Mark', 'Alex']), `    Expected outcome: Max, John and 2 others like this`)
+console.log(likes(['Max', 'John', 'Mark', 'Alex', 'Jacob']), `    Expected outcome: Max, John and 3 others like this`)
