f6d71014789b9207b4281a08e14491de78fa77ee
Christopher Jin <chrisjin1@gmail.com>
2023-01-12 12:24:23 -0500

    test cases

---
+++ 6kyus/js/convertStringToCamelCase.js
@@ -0,0 +1,10 @@
+// string input
+// Upper Camel Case
+//  if first word's first letter is capitalized, keep it that way
+// input string can either have dashes or underscores delimited
+
+function toCamelCase(str) {}
+
+console.log(toCamelCase('the-stealth-warrior'), 'theStealthWarrior')
+console.log(toCamelCase('The-Templar-warrior'), 'TheTemplarWarrior')
+console.log(toCamelCase('The-Templar-warrior'), 'TheTemplarWarrior')
