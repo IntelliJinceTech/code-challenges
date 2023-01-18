d7491a741b341c7a5988b80baab79345e454a94d
Christopher Jin <chrisjin1@gmail.com>
2023-01-18 12:28:08 -0500

    test cases

--- 6kyus/js/convertStringToCamelCase.js
+++ 6kyus/js/convertStringToCamelCase.js
@@ -3,7 +3,12 @@
 //  if first word's first letter is capitalized, keep it that way
 // input string can either have dashes or underscores delimited
 
-function toCamelCase(str) {}
+function toCamelCase(str) {
+	// Determine if string includes - or underscore
+	// Split on dash or underscore
+	// loop over each element in array and capitalize first character starting from index 1
+	//
+}
 
 console.log(toCamelCase('the-stealth-warrior'), 'theStealthWarrior')
 console.log(toCamelCase('The-Templar-warrior'), 'TheTemplarWarrior')
