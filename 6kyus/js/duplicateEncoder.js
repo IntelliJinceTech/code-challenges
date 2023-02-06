cfaa73547256095b24ec94c675ed3dcae1709bb3
Christopher Jin <chrisjin1@gmail.com>
2023-02-06 11:00:25 -0500

    prep complete for duplicate encoder pass 1

--- 6kyus/js/duplicateEncoder.js
+++ 6kyus/js/duplicateEncoder.js
@@ -6,11 +6,17 @@
 //      if character appears only once --> (
 //      if character appears >1 --> )
 //
-// ignore capitalization
+// ignore capitalization, could be a space
 //
 
 function duplicateEncode(word) {
-	//
+	//  TODO    Initalize an empty object
+	//  TODO    LOOP over the word
+	//  TODO        if character does not match property object, create key value pair of character:1
+	//  TODO        if character matches property in object, +=1 to value
+	//  TODO    LOOP over word again
+	//  TODO        if character has value >1, replace character with )
+	//  TODO        if character has value ===1, replace character with (
 }
 
 console.log(duplicateEncode('din')) //"((("
