a0dc801b24f98b362e1ee533e65d3249fbef227f
Christopher Jin <chrisjin1@gmail.com>
2023-02-08 09:49:12 -0500

    prep complete sort arrayby length 7kyu

---
+++ 7kyus/js/sortArrayByLength.js
@@ -0,0 +1,14 @@
+// array of strings - input
+// returns a sorted array - ordered from shortest to longest, containing the same strings
+
+function sortByLength(arr) {
+	// Todo ITERATE over input array
+	// Todo  initialize a variable
+	// Todo     Map (?) or sort? - that would return a new array
+	// todo     comparison of .length method of elements within the input array
+	// todo return the new array
+
+	return
+}
+
+console.log(sortByLength(['Beg', 'Life', 'I', 'To']))
