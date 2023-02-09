99dccbb8026ea494aa32736a72133cd22b086903
Christopher Jin <chrisjin1@gmail.com>
2023-02-09 13:23:06 -0500

    prep done for replace with alphabet position

---
+++ 6kyus/js/replaceWithAlphabetPosition.js
@@ -0,0 +1,10 @@
+// input string text - can have spaces , only alphabet
+// returns a string of numbers dilineated by spaces. spaces from the input don't count
+
+function alphabetPosition(text) {
+	// pseudocode
+	// initialize empty array / object
+	// if using array: create array - numbers of letters would be i + 1
+}
+
+console.log(alphabetPosition("The sunset sets at twelve o' clock."), '20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11')
