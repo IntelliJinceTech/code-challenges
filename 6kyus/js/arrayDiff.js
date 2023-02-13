01388abd306c5b0b349333c67e77586f5fc8a3f3
Christopher Jin <chrisjin1@gmail.com>
2023-02-13 13:11:40 -0500

    prep complete without pseudocode for array diff

---
+++ 6kyus/js/arrayDiff.js
@@ -0,0 +1,23 @@
+// inputs
+//      2 inputs:
+//      first input: array of numbers - can be empty
+//      second input: array of numbers - multiple elements or no elements
+
+// output
+//      one array or empty array if applicable
+
+// TODO
+// TODO
+// TODO
+// TODO
+// TODO
+
+function arrayDiff(a, b) {
+	//
+}
+
+console.log(arrayDiff([1, 2], [1])) //[2]
+console.log(arrayDiff([1, 2, 2], [1])) //[2,2]
+console.log(arrayDiff([1, 2, 2, 2, 2, 3], [2])) //[1,3]
+console.log(arrayDiff([], [1, 2])) //[]
+console.log(arrayDiff([1, 2, 3], [1, 2])) //[3]
