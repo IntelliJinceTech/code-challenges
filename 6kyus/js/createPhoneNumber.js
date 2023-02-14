c3fc38c73a7c32e1378a2f66d6b06e5ec222d1d2
Christopher Jin <chrisjin1@gmail.com>
2023-02-14 16:28:39 -0500

    prep complete for tribonacci sequence

--- 6kyus/js/createPhoneNumber.js
+++ 6kyus/js/createPhoneNumber.js
@@ -11,10 +11,19 @@
 // TODO   display into desired format
 // TODO 
 
+function createPhoneNumber1st(numbers) {
+  numbers=numbers.join('')
+  return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`
+}
+
 function createPhoneNumber(numbers) {
-  //
+  let format = '(xxx) xxx-xxxx'
+  for(let i = 0; i<numbers.length; i++) {
+    format = format.replace('x',numbers[i])
+  }
+  return format;
 }
 
 // ? examples
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])) // => returns "(123) 456-7890"
-console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])) // => returns "(111) 111-111"
+console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])) // => returns "(111) 111-1111"
---
+++ 6kyus/js/tribonacciSequence.js
@@ -0,0 +1,17 @@
+// given
+//  2 different inputs
+//    ? signature - starting point of the sequence
+//    ? length of the desired sequence including the signature
+
+function tribonacci(signature, n) {
+	//  TODO    initiate empty array
+	//  TODO    LOOP until length n
+	//  TODO        start at index 3
+	//  TODO        add last 3 numbers in array
+	//  TODO        push to empty array
+	//  TODO    combine array with signature array
+	//  TODO    return
+	//  TODO
+}
+
+console.log(tribonacci([1, 1, 1], 10)) // [1,1,1,3,5,9,17,31,57,105]
