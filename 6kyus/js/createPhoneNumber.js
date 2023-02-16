33bad67bfbaa084ed5ec10f33aceffb59605ec53
Christopher Jin <chrisjin1@gmail.com>
2023-02-16 12:58:19 -0500

    phone number kyu prep complete

---
+++ 6kyus/js/createPhoneNumber.js
@@ -0,0 +1,20 @@
+// given:
+//  array of 10 integers between 0 and 9
+//  can be in any order
+// return:
+//  string
+//  format in indexes: (012) 345-6789
+//  make sure to include the space after the closing parenthesis
+
+// TODO given an array of numbers
+// TODO   organize groups of elements together
+// TODO   display into desired format
+// TODO 
+
+function createPhoneNumber(numbers) {
+  //
+}
+
+// ? examples
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])) // => returns "(123) 456-7890"
+console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])) // => returns "(111) 111-111"
