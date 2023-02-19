e9f2412f1cbaaabccfd57ca7482b31d920dfdefa
Christopher Jin <chrisjin1@gmail.com>
2023-02-18 20:18:56 -0500

    odd Or Even 7kyus prep complete

---
+++ 7kyus/js/oddOrEven.js
@@ -0,0 +1,19 @@
+// given an array of integers , negative or positive are all fair game
+//  return a string either "odd" or "even"
+
+// input can be empty
+
+function oddOrEven() {
+  // code
+}
+// TODO given array of sum integers
+// TODO check if array has elements --> if not, return even
+// TODO   obtain sum
+// TODO   if 0 --> even
+// TODO   return even or odd depending on sum
+
+
+// examples
+console.log(oddOrEven([0])) //even
+console.log(oddOrEven([0,1,4])) // odd
+console.log(oddOrEven([-1023, 1, -2])) // even
