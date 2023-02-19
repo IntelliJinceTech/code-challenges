9e1256637449f267bc4a0613d73207cdec6c82b6
Christopher Jin <chrisjin1@gmail.com>
2023-02-19 13:39:17 -0500

    prep complete for ones and zeroes binary conversion

---
+++ 7kyus/js/onesAndZeroes.js
@@ -0,0 +1,18 @@
+//  parameters: array of some amount of elements (numbers)
+//  returns a number
+//  TODO  given array of numbers as input
+//  TODO    join them into a String
+//  TODO    parseInt on the string - test
+//  TODO
+
+const binaryArrayToNumber = arr => {
+  // 
+}
+
+
+
+console.log(binaryArrayToNumber([0,0,0,1])) // 1
+console.log(binaryArrayToNumber([0,0,1,0])) // 2
+console.log(binaryArrayToNumber([1,1,1,1])) // 15
+console.log(binaryArrayToNumber([1,0,1,1])) // 11
+
