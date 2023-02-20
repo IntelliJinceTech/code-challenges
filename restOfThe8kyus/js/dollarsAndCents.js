8dda06f9f4c2f6d3b0520b4fc05054747551610a
Christopher Jin <chrisjin1@gmail.com>
2023-02-20 14:39:28 -0500

    corrections with more solutions

--- restOfThe8kyus/js/dollarsAndCents.js
+++ restOfThe8kyus/js/dollarsAndCents.js
@@ -9,9 +9,20 @@
 // TODO Add $ to Stringified Number
 // TODO Return String
 
-function formatMoney() {
-
+function formatMoney(amount) { //3.1 length = 3
+  let deciDigits = String(amount).length - 1 - String(amount).indexOf('.') //1 
+  if(String(amount).indexOf('.') === -1) {
+    return `$${amount}.00`
+  } 
+  if(deciDigits === 1 ) {
+    return `$${amount}0`
+  }
+  return `$${amount}`
 }
 
 console.log(formatMoney(3)) // $3.00
 console.log(formatMoney(3.1)) // $3.10
+
+function formatMoneyCW(amount){
+  return '$' + amount.toFixed(2);
+}
