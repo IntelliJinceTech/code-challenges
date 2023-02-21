544083bc174b92f031aa69e65d278730abfe85ca
Christopher Jin <chrisjin1@gmail.com>
2023-02-21 08:57:16 -0500

    pseudocode complete for dollars and cents

--- restOfThe8kyus/js/dollarsAndCents.js
+++ restOfThe8kyus/js/dollarsAndCents.js
@@ -2,6 +2,13 @@
 // returns --> return string with $xx.xx
 //examples below
 
+// TODO check number of digits after . 
+// TODO   if none --> add 00
+// TODO   if 1 --> add 0
+// TODO   if 2 --> do nothing
+// TODO Add $ to Stringified Number
+// TODO Return String
+
 function formatMoney() {
 
 }
