05a1746e99a3e8348b56b40625390916ccb729ac
Christopher Jin <chrisjin1@gmail.com>
2023-02-22 08:54:06 -0500

    dollars and cents no pseudocode setup

---
+++ restOfThe8kyus/js/dollarsAndCents.js
@@ -0,0 +1,10 @@
+//parameters --> float with one or 2 digits after decimal point
+// returns --> return string with $xx.xx
+//examples below
+
+function formatMoney() {
+
+}
+
+console.log(formatMoney(3)) // $3.00
+console.log(formatMoney(3.1)) // $3.10
