b7132ca7171fc09b17e0261e15265b84bd42e3f7
Christopher Jin <chrisjin1@gmail.com>
2023-02-23 08:17:24 -0500

    prep other than pseudocode complete for number of Decimal digits

---
+++ 7kyus/js/numberOfDecimalDigits.js
@@ -0,0 +1,10 @@
+// parameters - number integer of some length 
+//  all inputs valid --> meaning?
+// returns - number of digits - Number
+
+function digits (n) {
+  //TODO  
+}
+
+console.log(digits(5)) // 1
+console.log(digits(12345)) // 5
