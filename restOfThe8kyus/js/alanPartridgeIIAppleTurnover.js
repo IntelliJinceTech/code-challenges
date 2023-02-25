0514e4ce2082ab21777031c3f8299755bf354ecf
Christopher Jin <chrisjin1@gmail.com>
2023-02-25 11:21:58 -0500

    alan partridge apple turnover square return string

---
+++ restOfThe8kyus/js/alanPartridgeIIAppleTurnover.js
@@ -0,0 +1,8 @@
+function apple(x) {
+  return x**2 > 1000 ? "It's hotter than the sun!!" : "Help yourself to a honeycomb Yorkie for the glovebox."
+}
+
+//parameters positive integer - could be in a string form
+// returns string
+
+console.log(apple("31")) 
