7a80d4f1788b8613c14a21487537cbc01383ef3f
Christopher Jin <chrisjin1@gmail.com>
2023-02-28 15:00:04 -0500

    prep complete for alternate capitalizations

---
+++ 7kyus/js/alternateCapitalization.js
@@ -0,0 +1,23 @@
+//  parameter is a string - input will be lower case and with no spaces
+//  returns 2 strings as output
+//    first string = evens + 0 will be capitalized
+//    second string = odds will be capitalized
+
+//TODO  initialize 2 empty strings into 2 variables
+//TODO  loop over string
+//TODO  when index = 0 --> add capitalized letter to first string and add lowercase letter to second string
+//TODO    OR index % 2 ===0 add capitalized letter to first string and add lowercase letter to second string
+//TODO    
+//TODO  when index % 2 ===1 --> add capitalized letter to second string and lowercase letter to first string
+//TODO  return array of first string and second string in separate indexes
+
+function capitalize(s){
+  return [];
+};
+
+// examples
+
+// console.log(capitalize("abcdef"))//,['AbCdEf', 'aBcDeF']);
+// console.log(capitalize("codewars"))//,['CoDeWaRs', 'cOdEwArS']);
+// console.log(capitalize("abracadabra"))//,['AbRaCaDaBrA', 'aBrAcAdAbRa']);
+// console.log(capitalize("codewarriors"))//,['CoDeWaRrIoRs', 'cOdEwArRiOrS']);
