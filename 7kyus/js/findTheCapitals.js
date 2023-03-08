9a4282ed240072fcd5b34ea423f7175f2352f864
Christopher Jin <chrisjin1@gmail.com>
2023-03-08 10:59:52 -0500

    prep for finding the capitals complete

---
+++ 7kyus/js/findTheCapitals.js
@@ -0,0 +1,17 @@
+//  single string parameter 
+  //  does it contains symbols or other characters?
+// returns an array of the indexes of all capital letters
+// examples
+
+function capitals(word) {
+  //todo  
+  //todo  
+  //todo  
+  //todo  
+  //todo  
+}
+
+// Examples:
+
+console.log(capitals('CodEWaRs')) // [0,3,4,6]
+console.log(capitals('jbBreQ')) // [2,5]
