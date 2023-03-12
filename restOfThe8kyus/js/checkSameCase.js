003e23c335ee40830d1775f02b1935f34e773b22
Christopher Jin <chrisjin1@gmail.com>
2023-03-11 20:31:00 -0500

    checkSame Case complete kata

--- restOfThe8kyus/js/checkSameCase.js
+++ restOfThe8kyus/js/checkSameCase.js
@@ -5,6 +5,50 @@
 //if letters are both the same case, return 1
 //otherwise return 0
 
-function sameCase(a, b) {
-    return 0;
+// function sameCase(a, b) {
+//     if (!isLetter(a) || !isLetter(b) || a.includes(":") || b.includes(":")) {
+//         return -1;
+//     }
+//     if (
+//         isUpperCase(a) === isUpperCase(b) ||
+//         isLowerCase(a) === isLowerCase(b)
+//     ) {
+//         return 1;
+//     } else {
+//         return 0;
+//     }
+// }
+
+function isLetter(string) {
+    return isNaN(string);
 }
+
+function isUpperCase(string) {
+    return string == string.toUpperCase();
+}
+
+function isLowerCase(string) {
+    return string == string.toLowerCase();
+}
+
+function sameCase(a, b) {
+    if (
+        a.toUpperCase() === a.toLowerCase() ||
+        b.toLowerCase() === b.toUpperCase()
+    ) {
+        return -1;
+    } else if (
+        (a === a.toLowerCase() && b === b.toLowerCase()) ||
+        (a === a.toUpperCase() && b === b.toUpperCase())
+    ) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+console.log(sameCase("b", 2));
+console.log(sameCase("C", "B"));
+console.log(sameCase("A", "s"));
+console.log(sameCase(" ", "z"));
+console.log(sameCase("H", ";"));
+// console.log(isLetter(":"));
