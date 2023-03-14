0a86cf7d89a960153e827b042653337f52d6edf8
Christopher Jin <chrisjin1@gmail.com>
2023-03-13 20:54:36 -0400

    checkSameCase prep work done

---
+++ restOfThe8kyus/js/checkSameCase.js
@@ -0,0 +1,10 @@
+//prep
+//how to tell is lowercase or uppercase?
+//check if both inputs are letters
+//check letters for uppercase or lower case
+//if letters are both the same case, return 1
+//otherwise return 0
+
+function sameCase(a, b) {
+    return 0;
+}
