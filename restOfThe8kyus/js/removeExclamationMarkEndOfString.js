a660c9371e7eb47d0c83023ba2f76f3cc821e677
Christopher Jin <chrisjin1@gmail.com>
2023-03-13 20:37:16 -0400

    prep remove exclamation point at end of string

---
+++ restOfThe8kyus/js/removeExclamationMarkEndOfString.js
@@ -0,0 +1,18 @@
+// given string - always a string
+// return string
+// remove("Hi!") == "Hi"
+// remove("Hi!!!") == "Hi!!"
+// remove("!Hi") == "!Hi"
+// remove("!Hi!") == "!Hi"
+// remove("Hi! Hi!") == "Hi! Hi"
+// remove("Hi") == "Hi"
+
+//psuedocode
+//? given a string
+//? if last character of string is !, return splice of string without last index
+//?
+
+function remove(string) {
+    //coding and coding....
+    return "";
+}
