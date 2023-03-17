a3cc5544c309a02d142ed87121d21260345ed3e6
Christopher Jin <chrisjin1@gmail.com>
2023-03-16 21:59:17 -0400

    vowelRemover complete

--- restOfThe8kyus/js/vowelRemover.js
+++ restOfThe8kyus/js/vowelRemover.js
@@ -3,11 +3,17 @@
 //examples: hello --> hll
 // pseudocode
 //? split string to array
-//? arr.filter out vowels
-//? return string
+//? arr method to filter out vowels
+//? return string.join
+
+const vowels = ["a", "e", "i", "o", "u"];
 
 function shortcut(string) {
-    return "";
+    let arrString = string.split("").filter((letter) => {
+        console.log(vowels.indexOf(letter));
+        return vowels.indexOf(letter) == -1;
+    });
+    return arrString.join("");
 }
 
 console.log(shortcut("hello"));
