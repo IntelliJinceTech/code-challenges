ba5cafd737359a7ae4bc87a3934138a32ad2bb1e
Christopher Jin <chrisjin1@gmail.com>
2023-04-07 20:28:04 -0400

    prep for what the real floor is

---
+++ restOfThe8kyus/js/whatTheRealFloor.js
@@ -0,0 +1,18 @@
+// parameters: integer, can be negative, no floats
+//return: integer, can be negative, no floats
+//examples:
+
+//psuedocode:
+//given an integer that can be positive or negative
+// if given floor is above 13, return given floor - 2
+//      else if given floor is above 0 return floor -1
+//      else return given floor
+
+function getRealFloor(n) {}
+
+console.log(getRealFloor(1));
+console.log(getRealFloor(0));
+console.log(getRealFloor(4));
+console.log(getRealFloor(15));
+console.log(getRealFloor(-2));
+console.log(getRealFloor(20));
