5598635d8a391b9ffb272f753cc8c64a98014fc6
Christopher Jin <chrisjin1@gmail.com>
2023-04-09 20:38:29 -0400

    two lowest positive integers complete 7kyu

--- 7kyus/js/sumOf2LowerstPosIntegers.js
+++ 7kyus/js/sumOf2LowerstPosIntegers.js
@@ -13,3 +13,10 @@
 //?     sort array lowest to highest
 //?     return sum of array[0] and array[1]
 //?
+
+function sumTwoSmallestNumbers(numbers) {
+    numbers = numbers.sort((a, b) => a - b);
+    return numbers[0] + numbers[1];
+}
+
+console.log(sumTwoSmallestNumbers([19, 5, 42, 2, 77]));
