bf64b7143fa9c2edfc7aaee66faddb40a1b43851
Christopher Jin <chrisjin1@gmail.com>
2023-04-09 20:31:46 -0400

    reorganization and prep for sum of 2 lowest positive integer 7kyu

---
+++ 7kyus/js/sumOf2LowerstPosIntegers.js
@@ -0,0 +1,15 @@
+//Parameters:
+// array - minimum of 4+ integers , length > 4, no floats,
+
+//return
+// returns single integer
+
+//examples:
+// input of [19,5,42,2,77] outputs ==> 7
+
+// pseudocode
+
+//? given an array
+//?     sort array lowest to highest
+//?     return sum of array[0] and array[1]
+//?
