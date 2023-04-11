f5bbabd93f258ba8a7ec3fea3085011fe1ea82dd
Christopher Jin <chrisjin1@gmail.com>
2023-04-10 21:51:45 -0400

    Lario Muigi Pipe Problem Complete

--- restOfThe8kyus/js/larioMuigiPipeProblem.js
+++ restOfThe8kyus/js/larioMuigiPipeProblem.js
@@ -4,7 +4,11 @@
 //examples down below
 
 function pipeFix(numbers) {
-    //todo
+    let newNumbers = [];
+    for (let i = numbers[0]; i <= numbers[numbers.length - 1]; i++) {
+        newNumbers.push(i);
+    }
+    return newNumbers;
 }
 
 console.log(pipeFix([1, 2, 3, 4, 5, 6, 8, 9])); //[1,2,3,4,5,6,7,8,9]
