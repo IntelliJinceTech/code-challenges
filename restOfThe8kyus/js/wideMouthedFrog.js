e885225f3913bc9f5a03e2cee94a672a5c25169b
Christopher Jin <chrisjin1@gmail.com>
2023-04-11 17:18:00 -0400

    prep for weird widemouth conditional exercise

---
+++ restOfThe8kyus/js/wideMouthedFrog.js
@@ -0,0 +1,14 @@
+//Parameters - takes in an animal string
+
+//return - string, either wide or small depending on input
+//examples:
+
+function mouthSize(animal) {
+    //todo given argument string
+    //todo check if alligator - if alligator return 'small'
+    //todo otherwise return 'wide'
+}
+
+console.log(mouthSize("toucan"));
+console.log(mouthSize("ant bear"));
+console.log(mouthSize("alligator"));
