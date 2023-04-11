dce48bfeb249f3bbe2d3045d1562644de7a070c7
Christopher Jin <chrisjin1@gmail.com>
2023-04-10 21:38:44 -0400

    larioMuigi Pipe Problem prep 8kyu

---
+++ restOfThe8kyus/js/larioMuigiPipeProblem.js
@@ -0,0 +1,12 @@
+//parameters - array of numbers
+//return - array of numbers that are consecutive between 0th index and the last index
+
+//examples down below
+
+function pipeFix(numbers) {
+    //todo
+}
+
+console.log(pipeFix([1, 2, 3, 4, 5, 6, 8, 9])); //[1,2,3,4,5,6,7,8,9]
+
+console.log(pipeFix([-1, 4])); //[-1,0,1,2,3,4]
