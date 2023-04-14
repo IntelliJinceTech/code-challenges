7f3875a615d630f4f93659ec6b16aee2c0f8ac9c
Christopher Jin <chrisjin1@gmail.com>
2023-04-14 16:09:16 -0400

    ends with exercise

---
+++ 7kyus/js/stringEndsWith.js
@@ -0,0 +1,13 @@
+//2 arguments - both strings check?
+// returns boolean
+function solution(str, ending) {
+    //todo
+    return str.endsWith(ending);
+    //todo
+    //todo
+}
+
+console.log(solution("abc", "bc")); // returns true
+console.log(solution("abc", "d")); // returns false
+console.log(solution("abcde", "cde")); //true
+console.log(solution("samurai", "ra")); //false
