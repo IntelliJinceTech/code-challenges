6189f38acd58134656a36cc18a7f5b2892fd0428
Christopher Jin <chrisjin1@gmail.com>
2023-04-15 17:17:59 -0400

    prep check if triangle complete

---
+++ 7kyus/js/isThisATriangle.js
@@ -0,0 +1,15 @@
+// takes in 3 integer values - check for strings or other values? no.
+// returns boolean
+// examples
+//pseudocode
+// check triangle sides
+// only a triangle if:
+//?     a+b>c
+//?     a+c>b
+//?     b+c>a
+function isTriangle(a, b, c) {
+    //todo
+}
+
+console.log(isTriangle(1, 2, 2)); //true
+console.log(isTriangle(7, 2, 2)); //false
