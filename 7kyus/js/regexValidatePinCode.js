cc5f6ae8d3072c94e7c656f84d9a92cf55175cc6
Christopher Jin <chrisjin1@gmail.com>
2023-04-20 15:19:20 -0400

    regex validate code pin with no regex prep complete

---
+++ 7kyus/js/regexValidatePinCode.js
@@ -0,0 +1,23 @@
+// pass in string or number - could be of any length and could be a number or string
+// returns boolean true or false
+
+function validatePIN(pin) {
+    // todo convert to Number (try to at least)
+    // todo check type so that it is a valid integer that is non-negative and non-float
+    // todo check length of Number
+    // todo
+    // todo
+}
+
+// returns true
+// console.log(validatePIN('1234'))
+// console.log(validatePIN(123456))
+
+// Returns false
+// console.log(validatePIN(12345))
+// console.log(validatePIN('a234'))
+// console.log(validatePIN('.234'))
+// console.log(validatePIN('-1234'))
+// console.log(validatePIN('-1.234'))
+
+console.log(Number('-1.1234')) //NaN
