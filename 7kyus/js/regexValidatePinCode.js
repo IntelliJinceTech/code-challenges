f95b62d71f434ec9cf593205da20cf86738ebec4
Christopher Jin <chrisjin1@gmail.com>
2023-04-20 16:23:40 -0400

    validate pin code without regex but a hack solution

--- 7kyus/js/regexValidatePinCode.js
+++ 7kyus/js/regexValidatePinCode.js
@@ -5,19 +5,41 @@ function validatePIN(pin) {
     // todo convert to Number (try to at least)
     // todo check type so that it is a valid integer that is non-negative and non-float
     // todo check length of Number
-    // todo
-    // todo
+
+    //return true or false
+    let pinLength = pin.length
+    if (pinLength != 4 && pinLength != 6) {
+        return false
+    }
+    for (let i in pin) {
+        if (pin[i] > '9' || pin[i] < '0') {
+            return false
+        }
+    }
+    return true
 }
 
-// returns true
+//     for (let i in pin) if (pin[i] > '9' || pin[i] < '0') return false
+//     return true
+// }
+
+//! returns true
 // console.log(validatePIN('1234'))
-// console.log(validatePIN(123456))
+// console.log(validatePIN('123456'))
+// console.log(validatePIN('000000'))
+// console.log(validatePIN('0123'))
 
-// Returns false
-// console.log(validatePIN(12345))
-// console.log(validatePIN('a234'))
-// console.log(validatePIN('.234'))
-// console.log(validatePIN('-1234'))
-// console.log(validatePIN('-1.234'))
+//! Returns false
+console.log(validatePIN(12345))
+console.log(validatePIN('a234'))
+console.log(validatePIN('.234'))
+console.log(validatePIN('-1234'))
+console.log(validatePIN('-1.234'))
+console.log(validatePIN('12.0'))
+console.log(validatePIN('001.'))
+console.log(validatePIN('123'))
 
-console.log(Number('-1.1234')) //NaN
+//? random testing
+// console.log('-1.1234' % 1)
+// console.log(5 % 1)
+// console.log('12.0'.includes('.'))
