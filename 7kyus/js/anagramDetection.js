43e4eb16757402e570f35975acd3d6c5784675b9
Christopher Jin <chrisjin1@gmail.com>
2023-04-27 11:34:40 -0400

    prep complete anagramdetection

---
+++ 7kyus/js/anagramDetection.js
@@ -0,0 +1,18 @@
+//  string parameters 
+  //  can there be symbols or not alphabetical characters?
+//  returns boolean
+//  examples below
+
+//todo  normalize both strings case-wise
+//todo  sort both strings alphabetically
+//todo  conditional between strings
+//todo  
+
+
+const isAnagram = (test,original) => {
+
+
+}
+
+console.log(isAnagram("foefet","toffee")) // true
+console.log("Buckethead","DeathCubeK") //true
