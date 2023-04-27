ccbe776f32be7a0f57a8bc421d306145f4505b0f
Christopher Jin <chrisjin1@gmail.com>
2023-04-27 11:43:15 -0400

    anagram detection exercise complete

--- 7kyus/js/anagramDetection.js
+++ 7kyus/js/anagramDetection.js
@@ -4,15 +4,22 @@
 //  examples below
 
 //todo  normalize both strings case-wise
+//todo  split to array
 //todo  sort both strings alphabetically
 //todo  conditional between strings
 //todo  
 
 
 const isAnagram = (test,original) => {
+  let testStr = test.toLowerCase().split('').sort().join('');
+  let originalStr = original.toLowerCase().split('').sort().join('');
+  return testStr == originalStr
 
+}
+const isAnagramOptimized = (test,original) => {
+  test.toLowerCase().split('').sort().join('') === original.toLowerCase().split('').sort().join('');
 
 }
 
-console.log(isAnagram("foefet","toffee")) // true
-console.log("Buckethead","DeathCubeK") //true
+console.log(isAnagramOptimized("foefet","toffee")) // true
+console.log(isAnagramOptimized("Buckethead","DeathCubeK")) //true
