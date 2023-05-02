9ebe206a9fc082869ab3433057df9fdd279552c7
Christopher Jin <chrisjin1@gmail.com>
2023-05-02 11:19:09 -0400

    string cleaning removing numericals prep complete

---
+++ restOfThe8kyus/js/stringCleaning.js
@@ -0,0 +1,11 @@
+// parameter - string - can contain numbers, could have lowercase or uppercase, and could have symbols and could have spaces
+// returns string with possibility of symbol characters - no numbers
+
+// TODO iterate over string argument
+// TODO check if character is a number
+// TODO remove numbers
+// TODO return string
+function stringClean(s) {
+}
+
+console.log(stringClean("A1 A1! AAA   3J4K5L@!!!"))//, "A A! AAA   JKL@!!!"))
