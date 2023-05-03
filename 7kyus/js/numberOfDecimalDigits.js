a6c0e4e003e9722482e658c17d9ec826e1e052f2
Christopher Jin <chrisjin1@gmail.com>
2023-05-03 09:30:28 -0400

    number of decimal digits exercise complete 7kyu

--- 7kyus/js/numberOfDecimalDigits.js
+++ 7kyus/js/numberOfDecimalDigits.js
@@ -2,11 +2,17 @@
 //  all inputs valid --> meaning?
 // returns - number of digits - Number
 
+//TODO  with parameter n, given that it is a valid number
+//TODO  convert to string
+//TODO  find length of string and return
 function digits (n) {
-  //TODO  with parameter n, given that it is a valid number
-  //TODO  convert to string
-  //TODO  find length of string and return
+  return `${n}`.length
 }
 
 console.log(digits(5)) // 1
 console.log(digits(12345)) // 5
+
+// other solutions
+function digitsCW(n) {
+  return n.toString().length;
+}
