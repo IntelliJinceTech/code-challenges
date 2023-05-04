a5ac6adfebc854fe0c9f7cec9456f333ffd7b9dd
Christopher Jin <chrisjin1@gmail.com>
2023-05-04 15:32:00 -0400

    sum of Odd Numbers in a triangle mathing

---
+++ 7kyus/js/sumOfOddNumbers.js
@@ -0,0 +1,24 @@
+// given a number input 
+// return a number
+
+function rowSumOddNumbers(n) {
+  //
+  return Math.pow(n,3)
+}
+
+// TODO Discover Pattern
+// TODO 
+// TODO 
+// TODO 
+
+// console.log(rowSumOddNumbers(42)) // 74088
+
+// 1 --> 1 
+// 2 --> 8
+// 3 --> 27
+// 4 --> 64
+
+console.log(Math.pow(2,3))
+console.log(Math.pow(3,3))
+console.log(Math.pow(4,3))
+console.log(Math.pow(42,3))
