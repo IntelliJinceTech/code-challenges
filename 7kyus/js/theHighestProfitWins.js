1b53c924f36ae6682147260d754f8ffc40bf78de
Christopher Jin <chrisjin1@gmail.com>
2023-05-07 21:36:12 -0400

    exercise complete highest profit wins exercise

--- 7kyus/js/theHighestProfitWins.js
+++ 7kyus/js/theHighestProfitWins.js
@@ -9,8 +9,10 @@
 // TODO 
 
 function minMax(arr) {
-  return 
+  return [Math.min(...arr),Math.max(...arr)]
+
 }
 
 console.log(minMax([1,2,3,4,5])) // [1,5]
-
+// console.log(minMax([2334454,5])) //--> [5,2334454]
+// console.log(minMax([1])) // --> [1,1]
