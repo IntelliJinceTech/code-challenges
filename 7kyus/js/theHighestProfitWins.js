c6b910de49785b15db8cfd45b7d1e600087d8a25
Christopher Jin <chrisjin1@gmail.com>
2023-05-07 21:28:29 -0400

    prep complete for highest profit wins exercise

---
+++ 7kyus/js/theHighestProfitWins.js
@@ -0,0 +1,16 @@
+// parameters - array of elements that consist of numbers
+// return an array of the Max and Min numbers
+
+// in the event of length 1, return the single element as both Max and Min (shouold be 2 elements in the returned array)
+// TODO taking an array
+// TODO   find the max, and min
+// TODO   return [max,min]
+// TODO 
+// TODO 
+
+function minMax(arr) {
+  return 
+}
+
+console.log(minMax([1,2,3,4,5])) // [1,5]
+
