ef7e8ebea790a3289b3df0456290aab5e789ccdb
Christopher Jin <chrisjin1@gmail.com>
2023-05-09 18:13:25 -0400

    completed exercise for tribonacci sequence with some refactoring

--- 6kyus/js/tribonacciSequence.js
+++ 6kyus/js/tribonacciSequence.js
@@ -3,15 +3,32 @@
 //    ? signature - starting point of the sequence
 //    ? length of the desired sequence including the signature
 
+//  TODO    initiate empty array with signature
+//  TODO    LOOP until length n
+//  TODO        start at index 3
+//  TODO        add last 3 numbers in array
+//  TODO        push to signature
+//  TODO    combine array with signature array
+//  TODO    return
+//  TODO
 function tribonacci(signature, n) {
-	//  TODO    initiate empty array
-	//  TODO    LOOP until length n
-	//  TODO        start at index 3
-	//  TODO        add last 3 numbers in array
-	//  TODO        push to empty array
-	//  TODO    combine array with signature array
-	//  TODO    return
-	//  TODO
+	let seqArr = [...signature] //[0,1,2]
+	for (let i = 2; i < n - 1; i++) {
+		let newArr = [seqArr[i - 2], seqArr[i - 1], seqArr[i]]
+		seqArr.push(newArr.reduce((acc, curr) => acc + curr, 0))
+	}
+	return seqArr.slice(0, n)
 }
 
-console.log(tribonacci([1, 1, 1], 10)) // [1,1,1,3,5,9,17,31,57,105]
+let triboacciCW = (signature, n) => {
+	for (var i = 0; i < n - 3; i++) {
+		// iterate n times
+		signature.push(signature[i] + signature[i + 1] + signature[i + 2]) // add last 3 array items and push to trib
+	}
+	return signature.slice(0, n) //return trib - length of n
+}
+
+console.table(tribonacci([1, 1, 1], 10)) // [1,1,1,3,5,9,17,31,57,105]
+console.table(tribonacci([0, 1, 1], 10)) // [0,1,1,2,4,7,13,24,44,81]
+console.table(tribonacci([1, 1, 1], 1)) // [1]
+console.table(tribonacci([300, 200, 100], 0)) // []
