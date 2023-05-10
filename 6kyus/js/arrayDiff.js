e770d8c1ad4789f295abef3245bab12a7afb42d7
Christopher Jin <chrisjin1@gmail.com>
2023-05-10 15:07:02 -0400

    exericse complete and added to anki

--- 6kyus/js/arrayDiff.js
+++ 6kyus/js/arrayDiff.js
@@ -21,11 +21,31 @@
 // TODO RETURN array a
 
 function arrayDiff(a, b) {
-	//
+	return a.filter(ele => {
+		// i want to return elements that don't match with array b
+		return !b.includes(ele) // b.includes (ele) means search array b if it includes the elements from a  .true if matching
+	})
 }
 
-console.log(arrayDiff([1, 2], [1])) //[2]
-console.log(arrayDiff([1, 2, 2], [1])) //[2,2]
-console.log(arrayDiff([1, 2, 2, 2, 2, 3], [2])) //[1,3]
-console.log(arrayDiff([], [1, 2])) //[]
-console.log(arrayDiff([1, 2, 3], [1, 2])) //[3]
+//? Alternatives
+function array_different(a, b) {
+	return a.filter(function (x) {
+		return b.indexOf(x) == -1
+	})
+}
+
+function array_diff(a, b) {
+	let arr = new Array()
+	for (let i = 0; i < a.length; i++) {
+		if (b.indexOf(a[i]) < 0) {
+			arr.push(a[i])
+		}
+	}
+	return arr
+}
+
+// console.log(arrayDiff([1, 2], [1])) //[2]
+// console.log(arrayDiff([1, 2, 2], [1])) //[2,2]
+// console.log(arrayDiff([1, 2, 2, 2, 2, 3], [2])) //[1,3]
+// console.log(arrayDiff([], [1, 2])) //[]
+// console.log(arrayDiff([1, 2, 3], [1, 2])) //[3]
