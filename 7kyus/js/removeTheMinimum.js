28150eab409f392c092e734e3be12014547661d8
Christopher Jin <chrisjin1@gmail.com>
2023-05-12 16:54:48 -0400

    remove smallest number in an array - splice 7kyu

--- 7kyus/js/removeTheMinimum.js
+++ 7kyus/js/removeTheMinimum.js
@@ -8,11 +8,16 @@
 //
 // examples below:
 
+//TODO  find the smallest number and get the index
+//TODO  splice the number out and return the new array
+//TODO
+//TODO
 function removeSmallest(numbers) {
-	//TODO  find the smallest number and get the index
-	//TODO  splice the number out and return the new array
-	//TODO
-	//TODO
+	let newArr = [...numbers]
+	newArr.splice(newArr.indexOf(Math.min(...numbers)), 1)
+	return newArr
 }
 
 console.log(removeSmallest([1, 2, 3, 4, 5])) // [2,3,4,5]
+console.log(removeSmallest([5, 3, 2, 1, 4])) //[5, 3, 2, 4]
+console.log(removeSmallest([2, 2, 1, 2, 1])) // [2, 2, 2, 1]
