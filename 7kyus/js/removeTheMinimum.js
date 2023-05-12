418b283bf04a3d9da75b8d0a7f761b53cf06bfe4
Christopher Jin <chrisjin1@gmail.com>
2023-05-12 16:24:58 -0400

    prep done for smallestNumber 7kyu

---
+++ 7kyus/js/removeTheMinimum.js
@@ -0,0 +1,18 @@
+// parameters: array of numbers in any order, numbers
+//
+// return:
+//  array of length -1 from input
+//  a new array without the minimum rating
+//  the order of elements should remain the same
+//  if multiple elements of the same minimum, remove the first one that comes up
+//
+// examples below:
+
+function removeSmallest(numbers) {
+	//TODO  find the smallest number and get the index
+	//TODO  splice the number out and return the new array
+	//TODO
+	//TODO
+}
+
+console.log(removeSmallest([1, 2, 3, 4, 5])) // [2,3,4,5]
