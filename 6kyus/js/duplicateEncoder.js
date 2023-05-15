ec9952c888adc88f603c75a4c064e5f1fbf8963f
Christopher Jin <chrisjin1@gmail.com>
2023-05-15 11:34:35 -0400

    prep done without pseudocode duplicate encoder

---
+++ 6kyus/js/duplicateEncoder.js
@@ -0,0 +1,17 @@
+// PREP
+// Parameters:
+//      string word
+// Return:
+//      string of parenthesis
+//      if character appears only once --> (
+//      if character appears >1 --> )
+//
+// ignore capitalization
+//
+
+function duplicateEncode(word) {
+	//
+}
+
+console.log(duplicateEncode('din')) //"((("
+console.log(duplicateEncode('recede')) // "()()()"
