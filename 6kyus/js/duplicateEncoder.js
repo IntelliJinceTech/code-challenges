c3e1ef61094013f48b614fa837d78c278be46c54
Christopher Jin <chrisjin1@gmail.com>
2023-05-16 13:22:56 -0400

    exercise complete duplicateEncode

--- 6kyus/js/duplicateEncoder.js
+++ 6kyus/js/duplicateEncoder.js
@@ -9,15 +9,43 @@
 // ignore capitalization, could be a space
 //
 
+//  TODO    Initalize an empty object
+//  TODO    LOOP over the word
+//  TODO        if character does not match property object, create key value pair of character:1
+//  TODO        if character matches property in object, +=1 to value
+//  TODO    LOOP over word again
+//  TODO        if character has value >1, replace character with )
+//  TODO        if character has value ===1, replace character with (
+
+// compObj['a'] = 1
+// compObj['a'] += 1
+// console.log(compObj.hasOwnProperty('a'))
+// console.log(compObj)
 function duplicateEncode(word) {
-	//  TODO    Initalize an empty object
-	//  TODO    LOOP over the word
-	//  TODO        if character does not match property object, create key value pair of character:1
-	//  TODO        if character matches property in object, +=1 to value
-	//  TODO    LOOP over word again
-	//  TODO        if character has value >1, replace character with )
-	//  TODO        if character has value ===1, replace character with (
+	const compObj = {}
+	for (let char of word.toLowerCase()) {
+		compObj.hasOwnProperty(char) ? (compObj[char] += 1) : (compObj[char] = 1)
+	}
+	// console.log(compObj)
+	return word
+		.toLowerCase()
+		.split('')
+		.map(ele => {
+			return compObj[ele] > 1 ? ')' : '('
+		})
+		.join('')
+}
+
+function duplicateEncoderCW(word) {
+	return word
+		.toLowerCase()
+		.split('')
+		.map(function (a, i, w) {
+			return w.indexOf(a) == w.lastIndexOf(a) ? '(' : ')'
+		})
+		.join('')
 }
 
 console.log(duplicateEncode('din')) //"((("
 console.log(duplicateEncode('recede')) // "()()()"
+console.log(duplicateEncode('(( @')) // "))(("
