bcf6a45b695ec88bb661c13d5c80fdd4ae2620a0
Christopher Jin <chrisjin1@gmail.com>
2023-05-18 15:47:39 -0400

    new folder for exercises plus one array diff exercise revisit

---
+++ ankiExercise/js/arrayDiff.js
@@ -0,0 +1,16 @@
+// parameters
+//      array of numbers as first input
+//      array of numbers as second input
+// return
+//      return array of numbers with all occurrences of number elements from second input removed
+// examples
+//      below
+// pseudocode
+
+function arrayDif(mainArr, removeArr) {
+	// TODO     LOOP over Main array
+	// TODO         if elements equal to any elements in removeArr, remove
+	// TODO
+}
+
+console.log(arrayDiff([1, 2, 2, 2, 3], [2])) // [1,3]
