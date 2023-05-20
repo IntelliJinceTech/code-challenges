702e687e2f7eae84d88316e27c8151d19285c002
Christopher Jin <chrisjin1@gmail.com>
2023-05-20 10:56:35 -0400

    prep kind of complete for latin squares

---
+++ 7kyus/js/latinSquares.js
@@ -0,0 +1,18 @@
+// input - number, no floats, positive only, not including 0
+// n sets of array of numbers,
+
+function makeLatinSquares(n) {
+	// code
+	// initialize an empty array
+	// LOOP from 1 to n  --- the number of elements in the array - the sets
+	//      return array
+	//      LOOP from 1 to n
+	//
+}
+
+console.log(makeLatinSquares(4), [
+	[1, 4, 3, 2],
+	[4, 3, 2, 1],
+	[1, 2, 3, 4],
+	[2, 1, 4, 3],
+])
