305fb32d8c74d8b08b83701adce08e1186b87f86
Christopher Jin <chrisjin1@gmail.com>
2023-05-22 13:36:24 -0400

    dontgivemefive prep complete

---
+++ 7kyus/js/dontGiveMeFive.js
@@ -0,0 +1,18 @@
+// start and end numbers as parameters
+//      start number is always less than end number
+//      negatives are allowed
+//      start and end numbers are both inclusive
+// return should be a number - it can have 5 in it
+//      it should be the count of all numbers without 5
+
+function dontGiveMeFive(start, end) {
+	//  given start and end numbers
+	// initialize an array of numbers between start and end numbers
+	// LOOP over array
+	// Need to remove any numbers with a 5 in it
+	// map, or filter
+	// .length method to count numbers
+}
+
+console.log(dontGiveMeFive(1, 9), 8)
+console.log(dontGiveMeFive(4, 16), 11)
