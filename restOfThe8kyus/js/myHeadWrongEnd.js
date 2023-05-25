de901a353ae07ad299900cb62fa6264632358f97
Christopher Jin <chrisjin1@gmail.com>
2023-05-25 10:40:27 -0400

    prep complete headWrongEnd Array manipulation

---
+++ restOfThe8kyus/js/myHeadWrongEnd.js
@@ -0,0 +1,12 @@
+// input: array of 3 values
+//      tail, body, head
+// output: array of 3 values - ordered head, body, tail
+//
+
+function fixTheMeerkat(arr) {
+	// take in array [0,1,2]
+	// reorder 0th index and index 2
+	// return
+}
+
+console.log(fixTheMeerkat(['tail', 'body', 'head']), ['head', 'body', 'tail'])
