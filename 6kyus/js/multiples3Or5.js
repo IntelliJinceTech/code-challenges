de6951588584e1f79f52f0a6aff9f3650fc9b1a5
Christopher Jin <chrisjin1@gmail.com>
2023-05-26 22:30:15 -0700

    prep minus pseudocode multiples 3 or 5 summing codewars

---
+++ 6kyus/js/multiples3Or5.js
@@ -0,0 +1,14 @@
+// number - both negative or positive
+//  if input is below 0 then return 0
+// outputs a number
+//  sum
+//
+//
+//
+//
+
+function solution(num) {
+	return
+}
+
+console.log(solution(10), 23)
