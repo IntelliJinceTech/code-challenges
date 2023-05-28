8486a4de19d35d25aea8aa862d8b93ae73606eed
Christopher Jin <chrisjin1@gmail.com>
2023-05-27 23:37:13 -0700

    prep complete multiples 3 or 5 summing codewars

--- 6kyus/js/multiples3Or5.js
+++ 6kyus/js/multiples3Or5.js
@@ -8,6 +8,15 @@
 //
 
 function solution(num) {
+	//todo  LOOP from 0 to num
+	//Todo      initialize sum variable with initial value of 0
+	//Todo      check if num is divisible by 3 and 5
+	//Todo      check if num is divisible by 3
+	//Todo      check if num is divisible by 5
+	//Todo      if conditions are met --> add to sum variable
+	//Todo
+	//Todo sum all multiples of 3 and 5
+
 	return
 }
 
