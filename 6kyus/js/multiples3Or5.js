aeb7bd0774e34c74c297f0020078a9262beb9a9a
Christopher Jin <chrisjin1@gmail.com>
2023-05-28 21:16:26 -0700

    multiples 3 or 5 final try

--- 6kyus/js/multiples3Or5.js
+++ 6kyus/js/multiples3Or5.js
@@ -19,9 +19,7 @@
 function solution(num) {
 	let sum = 0
 	for (let i = 0; i < num; i++) {
-		if (i % 3 === 0 && i % 5 === 0) {
-			sum += i
-		} else if (i % 3 === 0 || i % 5 === 0) {
+		if (i % 3 === 0 || i % 5 === 0) {
 			sum += i
 		}
 	}
