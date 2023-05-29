f01aa55ce5f1da36ec2450b433bd577c0d655c54
Christopher Jin <chrisjin1@gmail.com>
2023-05-28 21:07:44 -0700

    multiples 3 or 5 initial try

--- 6kyus/js/multiples3Or5.js
+++ 6kyus/js/multiples3Or5.js
@@ -7,17 +7,25 @@
 //
 //
 
+//todo if num <=0 return 0
+//todo  LOOP from 1 to num
+//Todo      initialize sum variable with initial value of 0
+//Todo      check if num is divisible by 3 and 5
+//Todo      check if num is divisible by 3
+//Todo      check if num is divisible by 5
+//Todo      if conditions are met --> add to sum variable
+//Todo
+//Todo sum all multiples of 3 and 5
 function solution(num) {
-	//todo  LOOP from 0 to num
-	//Todo      initialize sum variable with initial value of 0
-	//Todo      check if num is divisible by 3 and 5
-	//Todo      check if num is divisible by 3
-	//Todo      check if num is divisible by 5
-	//Todo      if conditions are met --> add to sum variable
-	//Todo
-	//Todo sum all multiples of 3 and 5
-
-	return
+	let sum = 0
+	for (let i = 0; i < num; i++) {
+		if (i % 3 === 0 && i % 5 === 0) {
+			sum += i
+		} else if (i % 3 === 0 || i % 5 === 0) {
+			sum += i
+		}
+	}
+	return sum
 }
 
 console.log(solution(10), 23)
