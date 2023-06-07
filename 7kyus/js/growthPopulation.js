729a8445786a5c1e38a31b99cdf234e714677990
Christopher Jin <chrisjin1@gmail.com>
2023-06-07 09:37:49 -0400

    prep done growth population no pseudo

---
+++ 7kyus/js/growthPopulation.js
@@ -0,0 +1,15 @@
+// 4 inputs all integers
+// check to see if they are integers? Any funny behavior that we need to account for like floats? or negatives? edge cases for inputs?
+// input order: (population at beginning of the year, percent growth, inhabitants coming or leaving each year, population (target) to equal or exceed)
+// input order with variables:
+//?     population at beginning of the year --> must be positive or 0
+//?     percent growth --> floating, integer, positive or null, floating number
+//?     inhabitants coming and going --> integer, positive or negative
+//?     target population --> greater than or equal to p
+function nbYear(p0, percent, aug, p) {
+	//
+}
+
+console.log(nbYear(1500, 5, 100, 5000), 15)
+console.log(nbYear(1500000, 2.5, 10000, 2000000), 10)
+console.log(nbYear(1500000, 0.25, 1000, 2000000), 94)
