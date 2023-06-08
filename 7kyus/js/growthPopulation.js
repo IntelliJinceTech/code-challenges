2a1e60923292109a48ae46f96bbb810cfbb665a2
Christopher Jin <chrisjin1@gmail.com>
2023-06-08 09:44:22 -0400

    prep done growth population

--- 7kyus/js/growthPopulation.js
+++ 7kyus/js/growthPopulation.js
@@ -7,9 +7,14 @@
 //?     inhabitants coming and going --> integer, positive or negative
 //?     target population --> greater than or equal to p
 function nbYear(p0, percent, aug, p) {
-	//
+	// initialize array with p0 at index 0
+	// do calculation and push to initialized array
+	// check when population exceeds or equals target population
+	// return index of the array
 }
 
-console.log(nbYear(1500, 5, 100, 5000), 15)
-console.log(nbYear(1500000, 2.5, 10000, 2000000), 10)
-console.log(nbYear(1500000, 0.25, 1000, 2000000), 94)
+// console.log(nbYear(1500, 5, 100, 5000), 15)
+// console.log(nbYear(1500000, 2.5, 10000, 2000000), 10)
+// console.log(nbYear(1500000, 0.25, 1000, 2000000), 94)
+
+console.log(Math.floor(1070 + 1070 * 0.02 + 50))
