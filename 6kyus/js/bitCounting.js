50bbac23239eb972f01ccf9069c456e17a4cd28d
Christopher Jin <chrisjin1@gmail.com>
2023-06-18 11:58:16 -0400

    first prep complete for counting Bits 6kyu

---
+++ 6kyus/js/bitCounting.js
@@ -0,0 +1,13 @@
+// takes in an integer and returns the number of bits represented as a number
+// does not take in negative integers
+
+function countBits(num) {
+	// given input number
+	// convert to binary
+	// calculate bits from binary
+}
+
+console.log(countBits(1234), `binary representation is 10011010010 and so the resulting bits should be 5`)
+console.log(countBits(4), 1)
+console.log(countBits(7), 3)
+console.log(countBits(9), 2)
