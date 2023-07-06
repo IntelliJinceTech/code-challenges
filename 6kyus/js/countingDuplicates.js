a1e0a85ca7f9c0dab2b1a9d51f645820e16a5d71
Christopher Jin <chrisjin1@gmail.com>
2023-07-06 08:36:39 -0400

    pre without pseudocode counting duplicates

---
+++ 6kyus/js/countingDuplicates.js
@@ -0,0 +1,12 @@
+// takes in a string - any unusual characters?
+//  only alphabets and numeric digits
+// returns number of distinct case-insensitive characters that occur more than once. (>1 or >=2)
+
+const duplicateCount = text => {
+	//
+}
+
+console.log(duplicateCount('abced'), `the answer is ${0} - no characters repeats more than once`)
+console.log(duplicateCount('aabBcde'), `the answer is ${2} - a occurs twice and b twice (b and B)`)
+console.log(duplicateCount('indivisibility'), `the answer is ${1} - i occurs 6 times`)
+console.log(duplicateCount('aA11'), `the answer is ${2} - a and 1`)
