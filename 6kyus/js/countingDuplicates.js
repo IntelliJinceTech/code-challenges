27206446b352296e67f8b06196eb594e707201f9
Christopher Jin <chrisjin1@gmail.com>
2023-07-07 08:45:59 -0400

    prep done counting duplicates

--- 6kyus/js/countingDuplicates.js
+++ 6kyus/js/countingDuplicates.js
@@ -2,9 +2,14 @@
 //  only alphabets and numeric digits
 // returns number of distinct case-insensitive characters that occur more than once. (>1 or >=2)
 
-const duplicateCount = text => {
-	//
-}
+// todo initiate an empty object
+// todo iterate over length of the string
+// todo     if character doesn't exist on object, create 'char': 1
+// todo     if character does exist on object, add 1 to char
+// todo loop over object
+// todo     filter keys by those that have more than 1 in value
+
+const duplicateCount = text => {}
 
 console.log(duplicateCount('abced'), `the answer is ${0} - no characters repeats more than once`)
 console.log(duplicateCount('aabBcde'), `the answer is ${2} - a occurs twice and b twice (b and B)`)
