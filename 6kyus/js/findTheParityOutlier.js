a5d7a5dfbddb7ccbac7b8896c8fb201b7a06f3f3
Christopher Jin <chrisjin1@gmail.com>
2023-07-11 13:47:44 -0400

    pseudocode done algorithems parityoutlier 2nd

--- 6kyus/js/findTheParityOutlier.js
+++ 6kyus/js/findTheParityOutlier.js
@@ -9,21 +9,20 @@
 // pseudocode
 // given an array of integers
 //  determine whether array is mostly even or mostly odd
-//      takes 3 loops to understand whether its even or odd
-//      initialize counter for counting even/odd.
-//      when even counter gets to 2 and iterations = 3 --> determines either even or odd
-//  use traversing method to find the opposite
-// if array is even, find odd
-// if array is odd, find even
+// traverse array
+// record whether num is even or odd, and append to correct odd/even array
+// if length of evenCounter is less than that of oddCounter, then return element in numCounter
 // return target element
 
 function findOutlier(integers) {
-	//your code here
+	let evenCounter = []
+	let oddCounter = []
+	let targetElement = 0
 }
 
 // examples
-console.log(findOutlier([0, 1, 2]), 1)
-console.log(findOutlier([1, 2, 3]), 2)
+// console.log(findOutlier([0, 1, 2]), 1)
+// console.log(findOutlier([1, 2, 3]), 2)
 console.log(findOutlier([2, 6, 8, 10, 3]), 3)
 console.log(findOutlier([0, 0, 3, 0, 0]), 3)
 console.log(findOutlier([1, 1, 0, 1, 1]), 0)
