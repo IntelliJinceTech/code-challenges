c22c9fcd7c300f384785b62a97409aedef3c8ef5
Christopher Jin <chrisjin1@gmail.com>
2023-07-13 14:50:19 -0400

    alphabet position with hash tables done 6kyu

--- 6kyus/js/replaceWithAlphabetPosition.js
+++ 6kyus/js/replaceWithAlphabetPosition.js
@@ -5,6 +5,28 @@ function alphabetPosition(text) {
 	// pseudocode
 	// initialize empty array / object
 	// if using array: create array - numbers of letters would be i + 1
+	// const alphabetRecord = 'abcdefghijklmnopqrstuvwxyz'.split('')
+	// const returnArr = text.split('').map(char => {
+	// 	if (alphabetRecord.includes(char)) {
+	// 		return alphabetRecord.indexOf(char) + 1
+	// 	}
+	// })
+	// return returnArr
+	const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+	const alphabetTable = {}
+
+	alphabet.split('').map((char, ind) => {
+		return (alphabetTable[char] = ind + 1)
+	})
+
+	return text
+		.toLowerCase()
+		.split('')
+		.map(letter => {
+			return alphabetTable[letter]
+		})
+		.filter(Boolean)
+		.join(' ')
 }
 
 console.log(alphabetPosition("The sunset sets at twelve o' clock."), '20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11')
