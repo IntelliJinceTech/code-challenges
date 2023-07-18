41d2b07d527d505b57f90e169e9619c5d58623b6
Christopher Jin <chrisjin1@gmail.com>
2023-07-18 12:26:02 -0400

    complete persistence while loop

--- 6kyus/js/persistentBugger.js
+++ 6kyus/js/persistentBugger.js
@@ -7,12 +7,20 @@
 function persistence(num) {
 	// pseudocode:
 	// create counter
-	// create a while loop that only runs when num is less than 10
+	// create a while loop that only runs until num is less than 10
 	//  convert num to string
 	//  string to array
 	//  reduce to new num
 	// return num
+	let counter = 0
+	while (num > 9) {
+		num = String(num)
+			.split('')
+			.reduce((acc, curr) => acc * curr)
+		counter++
+	}
+	return counter
 }
 
 console.log(persistence(39), 3)
-console.log(persistence(999), 4)
+// console.log(persistence(999), 4)
