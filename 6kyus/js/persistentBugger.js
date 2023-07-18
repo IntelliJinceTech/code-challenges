7ba5e4797ac4185ce19245afdb6e2d06672d395b
Christopher Jin <chrisjin1@gmail.com>
2023-07-18 12:01:22 -0400

    prep done for persistantBugger multiplicative

---
+++ 6kyus/js/persistentBugger.js
@@ -0,0 +1,18 @@
+// input is a positive integer
+// return single digit integer
+
+// multiplicative persistence is number of times you must multiply the digits in num until you reach a single digit
+// if num is single digit already then it should return 0
+
+function persistence(num) {
+	// pseudocode:
+	// create counter
+	// create a while loop that only runs when num is less than 10
+	//  convert num to string
+	//  string to array
+	//  reduce to new num
+	// return num
+}
+
+console.log(persistence(39), 3)
+console.log(persistence(999), 4)
