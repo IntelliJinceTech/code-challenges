364697a7b8922531417b7718a732011595b09e0a
Christopher Jin <chrisjin1@gmail.com>
2023-07-28 10:25:09 -0400

    pre pseudocode order please

---
+++ 6kyus/js/yourOrderPlease.js
@@ -0,0 +1,10 @@
+// string of words with a combination of letters and a number
+// should not expect to have more than one number digit per word - 1 thru 9
+// output should still include the number in the word
+// if input empty, output should be empty
+
+function order(words) {}
+
+console.log('is2 Thi1s T4est 3a', 'the answer is: ', 'Thi1s is2 3a T4est')
+console.log('4of Fo1r pe6ople g3ood th5e the2', 'the answer is: ', 'Fo1r the2 g3ood 4of th5e pe6ople')
+console.log('', 'the answer is: ', '', 'empty input should return empty string')
