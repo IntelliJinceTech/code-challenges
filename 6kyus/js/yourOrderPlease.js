c9bc6ad02d2fa94dd0a5cf7d08a86eecf9c87e18
Christopher Jin <chrisjin1@gmail.com>
2023-07-30 10:41:05 -0400

    added pseudocode for order please 6kyu

--- 6kyus/js/yourOrderPlease.js
+++ 6kyus/js/yourOrderPlease.js
@@ -3,6 +3,10 @@
 // output should still include the number in the word
 // if input empty, output should be empty
 
+// separate words of the string into an array separated by the spaces
+// filter out numbers
+// add words of said numbers to key
+// sort and assign words to sorted numbers
 function order(words) {}
 
 console.log('is2 Thi1s T4est 3a', 'the answer is: ', 'Thi1s is2 3a T4est')
