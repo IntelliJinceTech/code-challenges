6fba612566af759ed1469b9bf646012e5fe6031c
Christopher Jin <chrisjin1@gmail.com>
2023-07-31 11:09:10 -0400

    refactoring

--- 6kyus/js/yourOrderPlease.js
+++ 6kyus/js/yourOrderPlease.js
@@ -7,9 +7,17 @@
 // filter out numbers
 // add words of said numbers to key
 // sort and assign words to sorted numbers
-function order(words) {}
+function order(words) {
+	const wordKey = {}
+	words.split(' ').map(ele => {
+		// let num = ele.split('').filter(Number)
+		// wordKey[num] = ele
 
-console.log('is2 Thi1s T4est 3a', 'the answer is: ', 'Thi1s is2 3a T4est')
-console.log('4of Fo1r pe6ople g3ood th5e the2', 'the answer is: ', 'Fo1r the2 g3ood 4of th5e pe6ople')
-console.log('', 'the answer is: ', '', 'empty input should return empty string')
-console.log(words())
+		wordKey[ele.split('').filter(Number)] = ele
+	})
+	return Object.values(wordKey).join(' ')
+}
+
+console.log(order('is2 Thi1s T4est 3a'), 'the answer is: ', 'Thi1s is2 3a T4est')
+console.log(order('4of Fo1r pe6ople g3ood th5e the2'), 'the answer is: ', 'Fo1r the2 g3ood 4of th5e pe6ople')
+console.log(order(''), 'the answer is: ', '', 'empty input should return empty string')
