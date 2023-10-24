9c9d9bd18d60f35a343c5cda3465f96482826183
Christopher Jin <chrisjin1@gmail.com>
2023-10-24 17:24:27 -0400

    huntober challenge oct 24

---
+++ huntober2023/oct24.py
@@ -0,0 +1,21 @@
+# It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+# Return the average of the given array rounded down to its nearest integer.
+
+# The array will never be empty.
+
+# getAverage([2,2,2,2]),2)
+# getAverage([1,2,3,4,5,]),3);
+# getAverage([1,1,1,1,1,1,1,2]),1)
+
+    # given array input
+    # return average rounded down to its nearest integer
+    # get sum of all elements in list 
+    # get the length
+    # return the integer while rounding
+def getAverage(arr):
+    return int(sum(arr) / len(arr))
+
+print(getAverage([2,2,2,2]), 2)
+print(getAverage([1,2,3,4,5]), 3)
+print(getAverage([1,1,1,1,1,1,1,2]), 1)
