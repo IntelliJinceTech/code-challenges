381abbf43f99bba0f3c73d5c3e6c8bc20b3f36f9
Christopher Jin <chrisjin1@gmail.com>
2023-11-23 21:13:42 -0500

    complete training zeroes heroes

--- pythonExercises/8kyus/noZeroesForHeroes.py
+++ pythonExercises/8kyus/noZeroesForHeroes.py
@@ -10,7 +10,13 @@
 
 # split at 0 
 
+
+# iterate while number is divisible by 10
+# if divisible --> divide by 10
 def no_boring_zeroes(n):
-    return
+    
+    while n % 10 == 0:
+        n = n/10
+    return int(n)
 
 print(no_boring_zeroes(1450), 145)
