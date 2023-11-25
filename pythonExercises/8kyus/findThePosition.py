6151f93a2bec70e1724f29832ea595792cd3858b
Christopher Jin <chrisjin1@gmail.com>
2023-11-24 21:19:28 -0500

    pre pseudocode position alphabet 8kyu

---
+++ pythonExercises/8kyus/findThePosition.py
@@ -0,0 +1,8 @@
+# input letter --> convert to lowercase
+# output is string with position  "Position of alphabet:1"  (space after colon? yes)
+
+def position(alphabet):
+    pass
+
+print(position('a'), 'Position of alphabet: 1')
+print(position('z'), 'Position of alphabet: 26')
