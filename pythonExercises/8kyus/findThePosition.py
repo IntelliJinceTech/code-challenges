4f9970442bed2b1ea6d2dea844fed8818c8a14aa
Christopher Jin <chrisjin1@gmail.com>
2023-11-25 21:24:49 -0500

    first try position alphabet 8kyu

--- pythonExercises/8kyus/findThePosition.py
+++ pythonExercises/8kyus/findThePosition.py
@@ -1,8 +1,12 @@
 # input letter --> convert to lowercase
 # output is string with position  "Position of alphabet:1"  (space after colon? yes)
 
+
+# lowercase ascii string to list
+# or return difference between ascii numbers and a + 1?
+
 def position(alphabet):
-    pass
+    return f'Position of alphabet: {ord(alphabet) - ord("a") +1}' 
 
 print(position('a'), 'Position of alphabet: 1')
 print(position('z'), 'Position of alphabet: 26')
